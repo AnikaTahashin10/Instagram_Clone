9044fd270f98f2991e963023b98bec03c073de24
AnikaTahashin10 <anikatahashin90@gmail.com>
2024-02-22 20:00:10 +0200

    dark mode added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
 
 function App() {
 
   return (
     <>
-    hi
+    <Routes>
+      <Route path = '/' element= {<HomePage/>}/>
+    </Routes>
     </>
   )
 }
